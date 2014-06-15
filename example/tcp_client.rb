1132ad33d96c04869329ff37c75643f27e25bb37
Davide D'Agostino <info@daddye.it>
2014-06-15 01:42:28 -0700

    Work with fibers

--- example/tcp_client.rb
+++ example/tcp_client.rb
@@ -1,7 +1,6 @@
 require 'bundler/setup'
 require 'uv'
-
-# In c style
+require 'fiber'
 
 def refute_error(loop, error_code)
   return if error_code.nil?
@@ -17,10 +16,25 @@ err = UV.tcp_init(loop, client)
 refute_error(loop, err)
 
 # Connect
-addr = UV.ip4_addr('127.0.0.1', 4150)
-req = UV::Connect.new
-err = UV.tcp_connect(req, client, addr){ |r, s| p 'connected with status %d' % s; r }
-refute_error(loop, err)
+# addr = UV.ip4_addr('127.0.0.1', 4150)
+# req = UV::Connect.new
+# err = UV.tcp_connect(req, client, addr){ |r, s| p 'connected with status %d' % s; r }
+# refute_error(loop, err)
+
+def connect(loop, client)
+  f = Fiber.current
+  addr = UV.ip4_addr('127.0.0.1', 4150)
+  req = UV::Connect.new
+  err = UV.tcp_connect(req, client, addr){ |r, s| f.resume(r, s); r }
+  refute_error(loop, err)
+  Fiber.yield
+end
+
+Fiber.new do
+  r, s = connect(loop, client)
+  p r, s
+end.
+resume
 
 # RUN
 err = UV.run(loop, :uv_run_default)
